e0976305ea8203d87af9c82fbb7d23cc8d74c444
Simonin Lucas <lucas.simonin@cgi.com>
2017-05-16 21:50:42 +0200

    remove file

--- php/sampleMail.php
+++
@@ -1,7 +0,0 @@
-<?php
-	if($_SERVER['REQUEST_METHOD'] == "POST")
-	{
-		if(mail("momar.diop1@gmail.com", "Demande de contact!", "User: " . $_POST["firstname"] . " Email: " . $_POST["email"], $headers))
-                     echo "Votre message a bien été envoyé. Nous vous recontacterons dès que possible ! ";
-                else
-                   
